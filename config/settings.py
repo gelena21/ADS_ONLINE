71b8d7fb6635b50e316f7095aca300e08ed37608
gelena <helenatcitok@gmail.com>
2024-06-26 20:36:21 +0100

    fixing user password

--- config/settings.py
+++ config/settings.py
@@ -138,19 +138,6 @@ SWAGGER_SETTINGS = {
 DJOSER = {
     "SERIALIZERS": {"user_create": "users.serializers.UserRegistrationSerializer"},
     "LOGIN_FIELD": "email",
-    "USER_CREATE_PASSWORD_RETYPE": True,
-    "USERNAME_CHANGED_EMAIL_CONFIRMATION": True,
-    "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
-    "SEND_CONFIRMATION_EMAIL": True,
-    "SET_USERNAME_RETYPE": True,
-    "SET_PASSWORD_RETYPE": True,
-    "USERNAME_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
-    "PASSWORD_RESET_CONFIRM_URL": "email/reset/confirm/{uid}/{token}",
-    "ACTIVATION_URL": "activate/{uid}/{token}",
-    "SEND_ACTIVATION_EMAIL": True,
-    "SOCIAL_AUTH_TOKEN_STRATEGY": "djoser.social.token.jwt.TokenStrategy",
-    "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS": [
-        "your redirect url",
-        "your redirect url",
-    ],
 }
+
+AUTH_USER_MODEL = "users.User"
--- users/serializers.py
+++ users/serializers.py
@@ -8,6 +8,14 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     Сериализатор для регистрации пользователей.
     """
 
+    def create(self, validate_data):
+        user = User.objects.create(**validate_data)
+        user.set_password(user.password)
+        user.save()
+        return user
+
+
+
     class Meta:
         model = User
         fields = "__all__"
